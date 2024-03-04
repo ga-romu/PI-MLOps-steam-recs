054e44c1a7c9006cebbceeb439585ffbb7b8f648
ga-romu <garojas2@uc.cl>
2024-03-04 13:42:01 -0300

    13:41

--- main.py
+++ main.py
@@ -81,4 +81,19 @@ def developer_reviews_analysis(developer: str = Query(...,
 
 ##################################
 
+@app.get(path = '/recommendation_user',
+          description = """ <font color="blue">
+                        INSTRUCTIONS<br>
+                        1. Clik on "Try it out".<br>
+                        2. Type user_id the box box below.<br>
+                        3. Scroll to "Responses".
+                        </font>
+                        """,
+         tags=["General queries"])
+def recommendation_user(user_id: str = Query(..., 
+                                description="Id of specific User", 
+                                example="zombieman182")):
+        
+    return af.recommendation_user(user_id)
+
 ##################################
